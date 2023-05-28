4680f08df5cd8d88c36372b09cd334d344cc982a
e-ManueI <19648538+e-ManueI@users.noreply.github.com>
2023-05-28 18:21:43 +0300

    contact us styling

--- app/src/main/java/com/example/taskpro/ContactFragment.java
+++ app/src/main/java/com/example/taskpro/ContactFragment.java
@@ -1,19 +1,79 @@
 package com.example.taskpro;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ContactFragment extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ContactFragment extends Fragment implements OnMapReadyCallback {
+
+    private MapView mapView;
+    private GoogleMap mMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact, container, false);
+        View view = inflater.inflate(R.layout.fragment_contact, container, false);
+
+        // Initialize the MapView
+        mapView = view.findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker and move the camera to a specific location
+        LatLng location = new LatLng(-6.814018774740469, 39.28006551534149); // Replace with desired latitude and longitude
+        float zoomLevel = 12.0f; // Replace with desired zoom level
+        mMap.addMarker(new MarkerOptions().position(location).title("Dar Es Salaam Institute of Technology"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, zoomLevel));
+
+        // Set the marker click listener
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                // Zoom in when marker is clicked
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(marker.getPosition(), 17.0f));
+                return true;
+            }
+        });
     }
 }
