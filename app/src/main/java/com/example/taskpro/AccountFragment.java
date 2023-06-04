cc44a11d5cfbb5a774c91c92805e269f9e3db2ad
e-ManueI <19648538+e-ManueI@users.noreply.github.com>
2023-06-04 04:11:57 +0300

    Retrieve user information and display it in navigation and account

--- app/src/main/java/com/example/taskpro/AccountFragment.java
+++ app/src/main/java/com/example/taskpro/AccountFragment.java
@@ -10,13 +10,54 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AccountFragment extends Fragment implements View.OnClickListener {
+    private TextView userNameTextView;
+    private TextView userEmailTextView;
+    private TextView emailTextView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_account, container, false);
 
+        userNameTextView = rootView.findViewById(R.id.userName);
+        userEmailTextView = rootView.findViewById(R.id.userEmail);
+        emailTextView = rootView.findViewById(R.id.textView27);
+
+        // Retrieve user data from Firebase and update TextViews
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            String userEmail = currentUser.getEmail();
+            userEmailTextView.setText(userEmail);
+            emailTextView.setText(userEmail);
+
+            DatabaseReference userRef = FirebaseDatabase.getInstance().getReference().child("users").child(currentUser.getUid());
+            userRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        String userName = dataSnapshot.child("name").getValue(String.class);
+                        userNameTextView.setText(userName);
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    // Handle database error
+                }
+            });
+        }
+
+        // Find password layout and components
         ImageView passwordArrow = rootView.findViewById(R.id.imageChangePasswordArrow);
         LinearLayout passwordLayout = rootView.findViewById(R.id.passwordChange);
 
--- app/src/main/java/com/example/taskpro/DashboardActivity.java
+++ app/src/main/java/com/example/taskpro/DashboardActivity.java
@@ -16,12 +16,21 @@ import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class DashboardActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawerLayout;
     private TextView userNameTextView;
     private TextView userEmailTextView;
+
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +44,41 @@ public class DashboardActivity extends AppCompatActivity implements NavigationVi
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        // Initialize views
         View headerView = navigationView.getHeaderView(0);
         userNameTextView = headerView.findViewById(R.id.userName);
-        userEmailTextView = headerView.findViewById(R.id.userName);
+        userEmailTextView = headerView.findViewById(R.id.userEmail);
+
+
+        // Initialize Firebase components
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        // Retrieve the user's email from Firebase Authentication
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            String userEmail = user.getEmail();
+            userEmailTextView.setText(userEmail);
+        }
+
+        // Retrieve the user's name from Firebase Realtime Database
+        String userId = firebaseAuth.getCurrentUser().getUid();
+        DatabaseReference userRef = databaseReference.child("users").child(userId);
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    String userName = dataSnapshot.child("name").getValue(String.class);
+                    userNameTextView.setText(userName);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Handle error
+                Toast.makeText(DashboardActivity.this, "Failed to retrieve user data: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         userNameTextView.setOnClickListener(new View.OnClickListener() {
             @Override
